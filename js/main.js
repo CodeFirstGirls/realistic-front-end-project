2a4772c3a750b8a282f92e586320e20688936a44
Diana Lee <d1ana@protonmail.com>
2018-03-26 18:18:05 +0100

    wait until get request isdone

--- js/main.js
+++ js/main.js
@@ -3,33 +3,34 @@ var params = {
   userId: 1
 }
 
-$.get('https://jsonplaceholder.typicode.com/posts', params, function(data){
-  console.info("Total number of data objects received: ");
-  console.log(data.length);
-  console.info("Here is all the data: ");
-  console.log(data);
-  console.info("Here is the first object: ");
-  console.log(data[0]);
+$.get('https://jsonplaceholder.typicode.com/posts', params)
+  .done(function (data) {
+    console.info("Total number of data objects received: ");
+    console.log(data.length);
+    console.info("Here is all the data: ");
+    console.log(data);
+    console.info("Here is the first object: ");
+    console.log(data[0]);
 
-  var buffer = [];
-  var divOpening = "<div class='post row' id='post-0'>< h2 class='title' >";
+    var buffer = [];
+    var divOpening = "<div class='post row' id='post-0'>< h2 class='title' >";
 
-  // Here, we're using the first post as an example of how you can append this information
-  var firstPost = data[0];
-  buffer.push(divOpening + firstPost.title +
-    "<small> By User "+ firstPost.user + "</small></h2>"
-    + "<p>" + firstPost.body + "</p></div>");
+    // Here, we're using the first post as an example of how you can append this information
+    var firstPost = data[0];
+    buffer.push(divOpening + firstPost.title +
+      "<small> By User " + firstPost.user + "</small></h2>"
+      + "<p>" + firstPost.body + "</p></div>");
 
-  // var posts = data;
-  // console.log("total posts:", posts.length);
-  // posts.forEach(function(post){
-  //   console.log(post);
+    // var posts = data;
+    // console.log("total posts:", posts.length);
+    // posts.forEach(function(post){
+    //   console.log(post);
     // TODO: append posts to tables. 
     // Useful documentation for you http://api.jquery.com/category/manipulation/
-  // });
+    // });
 
-  $('#microblog .container').append(buffer);
-});
+    $('#microblog .container').append(buffer);
+  });
 
 
 // CREDIT for the API endpoint belongs to https://jsonplaceholder.typicode.com/
